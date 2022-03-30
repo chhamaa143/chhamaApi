403fb62e20fd9ffde8e800e5bca395c0f98d6f20
priya mehra <priyamehra6700@gmail.com>
2022-03-30 16:00:47 +0530

    hello

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ const cors = require('cors');
 const bodyParser =  require('body-parser');
 const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://chhama:parmar@cluster0.fznjr.mongodb.net/signupANDsingin?retryWrites=true&w=majority");
+app.use(cors());
+
 const indexRouter = require ('./routes/index');
 
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
-
-app.listen(3000,()=>{
+const port = process.env.PORT || 3000;s
+app.listen(port,()=>{
     console.log("server started");
 });
 module.exports=app;
